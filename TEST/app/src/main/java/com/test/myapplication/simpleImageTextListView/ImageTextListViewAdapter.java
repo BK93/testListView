e1fcd4dd1f548e4c930b43c3f97d7431423a92a6
BK93 <zxc3485@naver.com>
2016-03-30 23:27:41 +0900

    set url image

--- TEST/app/src/main/java/com/test/myapplication/simpleImageTextListView/ImageTextListViewAdapter.java
+++ TEST/app/src/main/java/com/test/myapplication/simpleImageTextListView/ImageTextListViewAdapter.java
@@ -1,9 +1,6 @@
 package com.test.myapplication.simpleImageTextListView;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,13 +9,10 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.test.myapplication.R;
 import com.test.myapplication.model.SimpleImageName;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -28,8 +22,10 @@ public class ImageTextListViewAdapter extends BaseAdapter {
 
     private ArrayList<SimpleImageName> simpleNames;
     private LayoutInflater inflater;
+    private Context context;
 
     public ImageTextListViewAdapter(@NonNull Context context) {
+        this.context = context;
         this.simpleNames = new ArrayList<>();
         this.inflater = LayoutInflater.from(context);
     }
@@ -63,38 +59,17 @@ public class ImageTextListViewAdapter extends BaseAdapter {
         TextView textView = (TextView) convertView.findViewById(R.id.text);
         textView.setText(data.getName());
 
-        if(android.os.Build.VERSION.SDK_INT > 9) {
-
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
-            StrictMode.setThreadPolicy(policy);
-
-        }
-        String url = data.getImage();
-        Bitmap bitmap = getBitmapFromURL(url);
-
         ImageView imageView = (ImageView) convertView.findViewById(R.id.image);
-        imageView.setImageBitmap(bitmap);
+
+        String url = data.getImage();
+
+        Picasso.with(context)
+                .load(url)
+                .resize(50, 50)
+                .centerCrop()
+                .into(imageView);
 
         return convertView;
-    }
-
-    public Bitmap getBitmapFromURL(String src) {
-        HttpURLConnection connection = null;
-        try {
-            URL url = new URL(src);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            Bitmap myBitmap = BitmapFactory.decodeStream(input);
-            return myBitmap;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            if (connection != null) connection.disconnect();
-        }
     }
 
 }
