bbe00aac2307954d6268eebab9b181675c4a2645
BK93 <zxc3485@naver.com>
2016-04-05 19:05:08 +0900

    remove joda

--- TEST/app/src/main/java/com/test/myapplication/MainActivity.java
+++ TEST/app/src/main/java/com/test/myapplication/MainActivity.java
@@ -12,32 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        leapYear();
-
         startActivity(MultipleCustomLayoutActivity.createIntent(this));
         finish();
     }
-
-//    public static void leapYear() {
-//        LocalDate localDate = new LocalDate();
-////        int year = localDate.getYear();
-//        int year = 2015;
-//
-//        Log.e("this year", "--> " + year);
-//        boolean isLeapYear = false;
-//
-//        if ((0 == (year % 4) &&
-//                0 != (year % 100)) ||
-//                0 == year % 400) {
-//            isLeapYear = true;
-//        } else {
-//            isLeapYear = false;
-//        }
-//
-//        if (isLeapYear) {
-//            Log.e("test-->", "윤년");
-//        } else {
-//            Log.e("test-->", "윤년아님");
-//        }
-//    }
 }
